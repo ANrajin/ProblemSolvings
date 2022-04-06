7f6d76fc711e98839fe23f543bfb72bca5bd8137
ANrajin <an.rajin@gmail.com>
2022-04-07 01:38:02 +0600

    updated

---
+++ UsingCSharp/CF-1512A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CF_1512A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            while (t > 0)
+            {
+                t--;
+
+                int n = int.Parse(Console.ReadLine());
+                string[] x = Console.ReadLine().Split();
+
+                int result = 1;
+
+                for(var i  = 0; i < x.Length; i++)
+                {
+                    if(int.Parse(x[i]) - int.Parse(x[i + 1]) != 0)
+                    {
+                        result += i;
+                        break;
+                    }
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
